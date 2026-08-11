28412af5fce8005197bdcce0bf93775af0a916aa
Guilherme Ribas <guitomas225@gmail.com>
2026-08-11 17:45:03 -0300

    ranking funcionado, falta modo noturno

--- trabalhoRevisao/main.c
+++ trabalhoRevisao/main.c
@@ -23,6 +23,7 @@ typedef struct
     double tempo;
     bool movimento_intervalo;
     bool status;
+    int ranking_atual[3];
 } estado_t;
 
 void configura_terminal()
@@ -100,12 +101,12 @@ void inicializa_estado(estado_t *est)
     est->municao = 30;
     est->escudo = ')';
     est->cont_escudos = 3;
-    est->inimigos_inativos = 20;
+    est->inimigos_inativos = 4;
     est->status = true;
     crono_inicia(&est->cronometro);
     inicializa_posicoes(est);
     est->movimento_intervalo = true;
-    est->tempo = 1.00;
+    est->tempo = 2.00;
 }
 
 void intervalo_movimento(estado_t *est)
@@ -283,17 +284,12 @@ void movimenta_inimigo(estado_t *est)
         }
     }
 }
-// inimigo_destroi_escudo(est);
 
-/*No final de uma onda (quando não houver mais ataques inativos e os ativos tiverem sido destruídos), os pontos são atualizados,
-e é apresentado um resumo do estado do jogo. Aguarda-se que o usuário digite r, e o número de tiros,
-ataques inimigos são reinicializados, o intervalo dos eventos temporais é atualizado, sorteia-se o tipo da nova onda (se diurna ou noturna) e a nova onda é iniciada.
-
-*/
 void atualiza_estado(estado_t *est)
-{   
+{
     est->municao = 30;
-    est->inimigos_inativos = 20;
+    est->arma_indice = 0;
+    est->inimigos_inativos = 4;
     double aumenta_tempo = (est->tempo * 10) / 100;
     est->tempo -= aumenta_tempo;
     est->status = true;
@@ -306,7 +302,7 @@ void atualiza_estado(estado_t *est)
 void resumo_onda(estado_t *est)
 {
     // pontos munição e ultima arma utilizada
-    printf("Total de pontos: %d ", est->pontos); // ainda tenho que ver com os escudos
+    printf("Total de pontos: %d ", est->pontos); 
     printf("Total de munição restante: %d \n", est->municao);
     printf("Ultima arma utilizada: %d ", est->arma_indice);
 }
@@ -329,7 +325,8 @@ void joga_onda(estado_t *est)
 
         printf("\nDigite r se quiser ir para a próxima onda:\n ");
         char q = '0';
-        while(q != 'r' && q!= 27){
+        while (q != 'r' && q != 27)
+        {
             q = lechar();
         }
         if (q == 'r')
@@ -337,7 +334,8 @@ void joga_onda(estado_t *est)
             atualiza_estado(est);
             system("clear");
         }
-        if(q == 27){
+        if (q == 27)
+        {
             est->status = false;
         }
     }
@@ -354,19 +352,102 @@ void joga_partida(estado_t *est)
     }
 }
 
+void le_ranking(estado_t *est)
+{
+    FILE *arq = fopen("ranking.txt", "r");
+
+    if (arq == NULL)
+    {
+        return;
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        fscanf(arq, "%d", &est->ranking_atual[i]);
+    }
+    fclose(arq);
+
+}
+
+bool verifica_atualizacao_ranking(estado_t *est)
+{
+    le_ranking(est);
+    int cont;
+    for (int i = 0; i < 3; i++)
+    {
+        if (est->pontos > est->ranking_atual[i])
+        {
+            for (int j = 2; j > i; j--)
+            {
+                est->ranking_atual[j] = est->ranking_atual[j - 1];
+            }
+
+            est->ranking_atual[i] = est->pontos;
+            return true;
+        }
+    }
+    return false;
+}
+
+void adiciona_ranking(estado_t *est)
+{
+    if (verifica_atualizacao_ranking(est))
+   {
+        FILE *arq = fopen("ranking.txt", "w");
+        if (arq == NULL)
+        {
+            return;
+        }
+
+        for (int i = 0; i < 3; i++)
+        {
+            fprintf(arq, "%d ", est->ranking_atual[i]);
+        }
+        // readiciona todos e confiro se o atual é maior que algum
+
+        // tenho que conferir se o que estou colocando é maio do que os que já estão
+        // posso colocar 0 no primeiro e ai quando fori conferir vai ver o0 e ir para o primeiro
+        fclose(arq);
+    }
+}
+
+void resumo_partida(estado_t *est)
+{
+    printf("\nTotal de Pontos da partida: %d \n", est->pontos);
+    adiciona_ranking(est);
+    printf("Ranking Atual:\n");
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%d. %d \n", i+1 , est->ranking_atual[i]);
+    }
+}
+
 int main()
 {
-    estado_t estado;
     configura_terminal();
     srand(time(NULL));
-
-    inicializa_estado(&estado);
-
-    // primeiro laço
-    while (estado.status)
+    estado_t estado;
+    char c;
+    do
     {
-        joga_partida(&estado);
-    }
+        c = ' ';
+        inicializa_estado(&estado);
 
+        // primeiro laço
+        while (estado.status)
+        {
+            joga_partida(&estado);
+        }
+        if (!estado.status)
+        {
+            resumo_partida(&estado);
+            printf("Deseja jogar novamente [S/N]:");
+            
+            while (c != 'S' && c != 'N')
+            {
+                c = lechar();
+            }
+        }
+
+    } while (c != 'N');
     normaliza_terminal();
 }
