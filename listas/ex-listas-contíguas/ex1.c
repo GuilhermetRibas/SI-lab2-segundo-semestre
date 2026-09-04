7d6d4a0dd1e0b56b5360f3c4f550c6f4f4d2a067
Guilherme Ribas <guitomas225@gmail.com>
2026-09-03 21:53:56 -0300

    listas contíguas

---
+++ listas/ex-listas-contíguas/ex1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+typedef int dado_t;
+
+typedef struct lista *Lista;
+
+struct lista {
+    int cap;      // quantos dados cabem em dad
+    int pri;      // onde em dad está o primeiro elemento da lista
+    int num;      // quantos elementos tem na lista
+    dado_t *dad;  // vetor de cap dados alocado dinamicamente
+};
+
+
+int main(){
+
+ 
+
+
+
+}
